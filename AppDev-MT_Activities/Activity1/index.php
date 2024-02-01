527082b7fc65c9e8578f1abad5aea0353b8c7000
Clark Belen <142368338+ClarkBelen@users.noreply.github.com>
2024-02-01 00:33:41 -0800

    Add files via upload

---
+++ AppDev-MT_Activities/Activity1/index.php
@@ -0,0 +1,185 @@
+<html>
+   <head>
+      <title>AppDev-MT_Activity1</title>	
+      <link rel ="stylesheet" href="css/bootstrap.min.css">
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.js"></script>
+   </head>
+   <body>
+      <h1>Activity 1</h1>
+
+      <!-- Table classes -->
+      <div class="container-fluid"> 
+         <table class="table">
+            <tr class="danger">
+               <th>ID</th>
+               <th>Name</th>
+               <th>Remarks</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>Alex</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>002</td>
+               <td>Cris</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>003</td>
+               <td>Jack</td>
+               <td class="danger">Failed</td>
+            </tr>
+         </table>
+         <table class="table table-striped">
+            <tr class="info">
+               <th>ID</th>
+               <th>Name</th>
+               <th>Remarks</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>Alex</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>002</td>
+               <td>Cris</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>003</td>
+               <td>Jack</td>
+               <td>Failed</td>
+            </tr>
+         </table>
+         <table class="table table-condensed">
+            <tr>
+               <th>ID</th>
+               <th>Name</th>
+               <th>Remarks</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>Alex</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>002</td>
+               <td>Cris</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>003</td>
+               <td>Jack</td>
+               <td>Failed</td>
+            </tr>
+         </table>
+         <table class="table table-hover table-bordered">
+            <tr>
+               <th>ID</th>
+               <th>Name</th>
+               <th>Remarks</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>Alex</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>002</td>
+               <td>Cris</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>003</td>
+               <td>Jack</td>
+               <td>Failed</td>
+            </tr>
+         </table>
+         <table class="table table-bordered">
+            <tr>
+               <th>ID</th>
+               <th>Name</th>
+               <th>Remarks</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>Alex</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>002</td>
+               <td>Cris</td>
+               <td>Passed</td>
+            </tr>
+            <tr>
+               <td>003</td>
+               <td>Jack</td>
+               <td>Failed</td>
+            </tr>
+         </table>
+      </div>
+
+      <hr/>
+      <br/>
+      <!-- Typography and Text Styles -->
+      <div class="container">
+         <p class="text-success bg-success">Success</p>
+         <p class="text-danger bg-danger">Danger</p>
+         <p class="text-info bg-info">Info</p>
+         <p class="text-primary bg-primary">Primary</p>
+         <p class="text-muted">Muted</p>
+         <p class="text-warning bg-warning">Warning</p>
+
+         <p class="lead">Information Technology</p>
+         <p class="small">Information Technology</p>
+         <p class="text-center">Information Technology</p>
+         <p class="text-right">Information Technology</p>
+         <p class="text-uppercase">Information Technology</p>
+         <p class="text-lowercase">Information Technology</p>
+
+      </div>
+
+      <hr/>
+      <br/>
+      <!-- Bootstrap Grid System -->
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-1" style="background: green;">grid</div>
+            <div class="col-md-1" style="background: red;">grid</div>
+            <div class="col-md-5" style="background: blue;">grid</div>
+         </div>
+
+         <div class="row">
+            <div class="col-md-6" style="background: orange;">grid</div>
+         </div>
+      </div>
+
+      <hr/>
+      <br/>
+      <!-- Container classes -->
+      <div class="container-fluid">
+         <header class="jumbotron">
+            <h1>Container fluid</h1>
+         </header>
+      </div>
+
+      <hr/>
+      <br/>
+      <div class="container">
+         <header class="jumbotron">
+            <h1>Container</h1>
+         </header>
+      </div>
+
+      <hr/>
+      <br/>
+      <div class="jumbotron">
+         <h1>Hello</h1>
+
+      </div>
+   </body>
+</html>
+
